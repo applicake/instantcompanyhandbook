d2817b87693ee7c38c547d1968bd308fc48d0162
Bartlomiej Niemtur <bartek.n@applicake.com>
2011-03-23 14:46:20 +0100

    added spawn plugin, moved pdf generation to background job

--- app/models/handbook.rb
+++ app/models/handbook.rb
@@ -11,27 +11,31 @@ class Handbook < ActiveRecord::Base
   end
 
   def generate_pdf
+
     # generating pdf file through prawn
-    # todo:? move that to background job
+    # via a background job using spawn
+    # https://github.com/tra/spawn
+    spawn do
 
-    pdf = Prawn::Document.new(
-      :page_size => [500, 1000],
-      :page_layout => :landscape,
-      :left_margin => 0, 
-      :right_margin => 0,
-      :top_margin => 0,
-      :bottom_margin => 0
-    )
+      pdf = Prawn::Document.new(
+        :page_size => [500, 1000],
+        :page_layout => :landscape,
+        :left_margin => 0, 
+        :right_margin => 0,
+        :top_margin => 0,
+        :bottom_margin => 0
+      )
 
-    pdf.text "#{name}"
-    Photo.all.sort_by {|p| p.order} .each do |photo|
-      pdf.start_new_page
-      pdf.image "#{photo.data.path}", :at => [0, 500], :width => 1000, :height => 500
+      pdf.text "#{name}"
+      Photo.all.sort_by {|p| p.order} .each do |photo|
+        pdf.start_new_page
+        pdf.image "#{photo.data.path}", :at => [0, 500], :width => 1000, :height => 500
+      end
+
+      p = "public/handbooks/#{id}"
+      Dir.mkdir(p) unless File.directory?(p)
+      pdf.render_file "#{p}/corporate_handbook.pdf"
     end
-
-    p = "public/handbooks/#{id}"
-    Dir.mkdir(p) unless File.directory?(p)
-    pdf.render_file "#{p}/corporate_handbook.pdf"
   end
 
 end

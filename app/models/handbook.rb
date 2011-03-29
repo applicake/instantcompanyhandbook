69d45435f17f5a75e5fbaebcc2553e64b60f5987
Bartlomiej Niemtur <bartek.n@applicake.com>
2011-03-29 10:59:57 +0200

    replaced spawn with delay_job

--- app/models/handbook.rb
+++ app/models/handbook.rb
@@ -15,66 +15,55 @@ class Handbook < ActiveRecord::Base
     self.id = UUIDTools::UUID.timestamp_create().to_s
   end
 
-  # Creating pdf through prawn
-  # via a background job using spawn.
-  # (Let's hope it's okay;
-  # if done other way,
-  # it might bring the whole server down) # just kidding
-  #
-  # https://github.com/tra/spawn
   def generate_pdf
-    # about 'nice': http://en.wikipedia.org/wiki/Nice_(Unix)
-    # argv is for naming a process (e.g. for ps aux)
-    spawn :nice => 5, :argv => "spawn #{id}" do
+    pdf = Prawn::Document.new(
+      :page_size => [733, 1000],
+      :page_layout => :landscape,
+      :left_margin => 0, 
+      :right_margin => 0,
+      :top_margin => 0,
+      :bottom_margin => 0
+    )
 
-      pdf = Prawn::Document.new(
-        :page_size => [733, 1000],
-        :page_layout => :landscape,
-        :left_margin => 0, 
-        :right_margin => 0,
-        :top_margin => 0,
-        :bottom_margin => 0
-      )
-
-      pdf.bounding_box [50,633], :width => 900, :height => 533 do
-        pdf.bounding_box [0,533], :width => 900, :height => 200 do
-          pdf.text(
-            "#{name}'s Corporate Handbook", 
-            :size => 32, 
-            :align => :center, 
-            :valign => :center
-          )
-        end
-        pdf.bounding_box [100,333], :width => 700, :height => 333 do
-          [ """
-          We crated this page because we observed a dangerous lack of structure in the corporate world, particularly in the fragile environment of startup ecosystem.
-          """, """
-          Startups ignore necessary bureaucracy, their values are only vaguely defined and, henceforth, they are not successful in executing them.
-          """, """
-          We believe following a set rudimentary rules it necessary to overcome the forthcoming challenges, maintain efforts to deliver top execution, ensure peak performance of the workforce, generate passion, involvement and achievement. Finally, maintain motivation to design and deliver market-leading products and solutions.
-          ""","""
-          And remember the meta-rule: \"Fake it until you make it!\"
-          """ ].each do |paragraph|
-            pdf.text paragraph, :align => :left, :size => 16
-          end
+    pdf.bounding_box [50,633], :width => 900, :height => 533 do
+      pdf.bounding_box [0,533], :width => 900, :height => 200 do
+        pdf.text(
+          "#{name}'s Corporate Handbook", 
+          :size => 32, 
+          :align => :center, 
+          :valign => :center
+        )
+      end
+      pdf.bounding_box [100,333], :width => 700, :height => 333 do
+        [ """
+        We crated this page because we observed a dangerous lack of structure in the corporate world, particularly in the fragile environment of startup ecosystem.
+        """, """
+        Startups ignore necessary bureaucracy, their values are only vaguely defined and, henceforth, they are not successful in executing them.
+        """, """
+        We believe following a set rudimentary rules it necessary to overcome the forthcoming challenges, maintain efforts to deliver top execution, ensure peak performance of the workforce, generate passion, involvement and achievement. Finally, maintain motivation to design and deliver market-leading products and solutions.
+        ""","""
+        And remember the meta-rule: \"Fake it until you make it!\"
+        """ ].each do |paragraph|
+          pdf.text paragraph, :align => :left, :size => 16
         end
       end
-
-      Photo.all.sort_by {|p| p.order} .each do |photo|
-        pdf.start_new_page
-        pdf.image "#{photo.data.path}", :at => [0, 733], :width => 1000, :height =>733 
-        if logo.exists?
-          pdf.image logo.path, :position => :right, :vposition => :bottom
-        end
-      end
-
-      p = "public/handbooks/#{id}"
-      Dir.mkdir(p) unless File.directory?(p)
-      pdf.render_file "#{p}/corporate_handbook.pdf"
-
-      HandbookMailer.availability_notification(self).deliver if email
-
     end
+
+    Photo.all.sort_by {|p| p.order} .each do |photo|
+      pdf.start_new_page
+      pdf.image "#{photo.data.path}", :at => [0, 733], :width => 1000, :height =>733 
+      if logo.exists?
+        pdf.image logo.path, :position => :right, :vposition => :bottom
+      end
+    end
+
+    p = "public/handbooks/#{id}"
+    Dir.mkdir(p) unless File.directory?(p)
+    pdf.render_file "#{p}/corporate_handbook.pdf"
+
+    HandbookMailer.availability_notification(self).deliver if email
+
   end
+  handle_asynchronously :generate_pdf
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110328085048) do
+ActiveRecord::Schema.define(:version => 20110329084655) do
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "handbooks", :id => false, :force => true do |t|
     t.string   "id",                :limit => 36

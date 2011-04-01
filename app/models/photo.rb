bd52576addd3f2d2e6e600b3095d0b4ff29161ac
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-04-01 12:52:50 +0200

    added custom logo (techcrunch) to preview book

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,18 @@
+require 'paperclip_processors/watermark'
+
 class Photo < ActiveRecord::Base
 
   attr_accessible :data, :order, :logo_overlay
 
-  has_attached_file :data, :styles => {:large => '1000x733!', :thumbnail => '256x256#'}, :default_style => :large
+  # paperclip-held image file, and its attributes;
+  # note: watermark is a custom processor, can be found 
+  # in the ./lib/paperclip_processors/watermark.rb
+  has_attached_file :data, :processors => [:watermark], :styles => {
+    :large => '1000x733!', 
+    :thumbnail => '256x256#', 
+    # the image with an overlay logo
+    :tcrl => {:geometry => '1000x733!', :watermark_path => "#{Rails.public_path}/images/tcrl_overlay.png", :position => 'SouthEast'}
+  }, :default_style => :large
 
   validates_attachment_presence :data
   # validates_attachment_size :data, :less_than => 5.megabytes  

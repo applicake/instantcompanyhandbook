f77f0db1e74f086d13550494ec8292d7bf336b35
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-04-04 13:57:06 +0200

    moved recaptcha keys to ./config/recaptcha.yml

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,10 @@
+# recaptcha keys - kept in an .yml file, loaded selectively, 
+# environment independant; sample file: /config/recaptcha.yml :
+#
+# public_key: 111111111111111111111
+# private_key: 222222222222222222222
 Recaptcha.configure do |config|
-  config.public_key  = '6Lce7MISAAAAAN-nQU-RaGidOuC69gZD7tTYhFoo'
-  config.private_key = '6Lce7MISAAAAAOINSERT_PASSWORD_HEREvhcv5CZbm9'
+  keys = YAML.load_file("#{Rails.root}/config/recaptcha.yml").symbolize_keys
+  config.public_key = keys[:public_key]
+  config.private_key = keys[:private_key]
 end

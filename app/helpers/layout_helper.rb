0227f15d446966b0c2a6e8020bfef3431315e386
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2011-04-05 10:45:53 +0200

    created analytics helpers, moved account id to configuration files

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,5 +1,32 @@
 module LayoutHelper
 
+  # returns the google analytics account id; should be set
+  # in the config/passwords.yml (as some other crutial
+  # attributes)
+  def analytics_account
+    YAML.load_file("#{Rails.root}/config/passwords.yml")['analytics']['account']
+  end
+
+  # puts the google analytics tracking script
+  def analytics_tracking account
+    haml_tag :script, :type => "text/javascript" do
+      haml_concat """
+      //<![CDATA[
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{account}']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+      //]]>
+      """
+    end
+  end
+
+
   # prints notice/alert variable, if they are being
   # sent to the view from the controller
   def alerts
